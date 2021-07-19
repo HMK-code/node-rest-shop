24d2af95f53f7528f6fcb92cb613385314ae57a7
Hamdi <HMKepenek@gmail.com>
2021-07-19 17:01:56 +0300

    first commit

--- API/routes/models/product.js
+++ API/routes/models/product.js
@@ -2,8 +2,21 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    price: Number
+    name: {type: String} ,
+    price: { type: Number}
 });
 
-module.exports = mongoose.model('Product', productSchema);
+ module.exports = mongoose.model('Product', productSchema);
+
+/*  module.exports = async (arg1, arg2, arg3) => {
+
+    await mongo().then(async mongoose => {
+        try{
+            console.log('Connected to mongo!!');
+            await command.execute(client, message, args);
+        }
+        finally{
+            mongoose.connection.close();
+        }
+    });
+}; */
--- app.js
+++ app.js
@@ -3,32 +3,50 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const fetch = require("node-fetch")
 
 const productRoutes = require("./API/routes/products")
 const orderRoutes = require("./API/routes/orders")
 
-mongoose.connect("mongodb+srv://new-user-31:new-user-31" + process.env.MONGO_ATLAS_PW + "@node-rest-shop.qkgco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+
+
+/* const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://new-user-31:new-user-31@node-rest-shop.qkgco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+ */
+
+ mongoose.connect("mongodb://localhost:27017/company",{useMongoClient:true}) 
+ mongoose.Promise = global.Promise;
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*"); // @@
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    res.header("Access-Control-Allow-Methods", "PUT , POST , PATCH , DELETE , GET");
     return res.status(200).json({});
   }
+  console.log(req.originalUrl);
   next();
 });
 
 // Routes which should handle requests
+
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
